dc54aee09f3f4436d2ea677ba29e88eaf26363b4
AliRaza <32015381+AliTechGeek52@users.noreply.github.com>
2019-06-19 19:19:20 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -16,74 +16,142 @@
 
     // for first product
 
-    function increaseValue() {
-  var value = parseInt(document.getElementById('number').value, 10);
-  value = isNaN(value) ? 0 : value;
-  value++;
-  document.getElementById('number').value = value;
+//     function increaseValue() {
+//   var value = parseInt(document.getElementById('number').value, 10);
+//   value = isNaN(value) ? 0 : value;
+//   value++;
+//   document.getElementById('number').value = value;
+// }
+
+// function decreaseValue() {
+//   var value = parseInt(document.getElementById('number').value, 10);
+//   value = isNaN(value) ? 0 : value;
+//   value < 1 ? value = 1 : '';
+//   value--;
+//   document.getElementById('number').value = value;
+// }
+
+// // for second product
+//     function increaseValueTwo() {
+//   var value = parseInt(document.getElementById('number-two').value, 10);
+//   value = isNaN(value) ? 0 : value;
+//   value++;
+//   document.getElementById('number-two').value = value;
+// }
+
+// function decreaseValueTwo() {
+//   var value = parseInt(document.getElementById('number-two').value, 10);
+//   value = isNaN(value) ? 0 : value;
+//   value < 1 ? value = 1 : '';
+//   value--;
+//   document.getElementById('number-two').value = value;
+// }
+
+// // For third product
+// function increaseValueThree() {
+//   var value = parseInt(document.getElementById('number-three').value, 10);
+//   value = isNaN(value) ? 0 : value;
+//   value++;
+//   document.getElementById('number-three').value = value;
+// }
+
+// function decreaseValueThree() {
+//   var value = parseInt(document.getElementById('number-three').value, 10);
+//   value = isNaN(value) ? 0 : value;
+//   value < 1 ? value = 1 : '';
+//   value--;
+//   document.getElementById('number-three').value = value;
+// }
+
+var val = 0, purchase = 0, count_ap = 0, count_ban = 0, count_org = 0, total_purchase = 0;
+function values() {
+    val = document.querySelector("input").value;
 }
-
-function decreaseValue() {
-  var value = parseInt(document.getElementById('number').value, 10);
-  value = isNaN(value) ? 0 : value;
-  value < 1 ? value = 1 : '';
-  value--;
-  document.getElementById('number').value = value;
+function result() {
+    document.querySelector("strong").innerHTML = total_purchase;
 }
+function increaseValue() {
 
-// for second product
-    function increaseValueTwo() {
-  var value = parseInt(document.getElementById('number-two').value, 10);
-  value = isNaN(value) ? 0 : value;
-  value++;
-  document.getElementById('number-two').value = value;
-}
+    if (val >= 10) {
+        count_ap += 1;
+        purchase += 10;
+        val = val - purchase;
+        total_purchase = purchase + total_purchase;
+        purchase = 0;
+        document.querySelector("#number").value = count_ap;
+        result();
 
-function decreaseValueTwo() {
-  var value = parseInt(document.getElementById('number-two').value, 10);
-  value = isNaN(value) ? 0 : value;
-  value < 1 ? value = 1 : '';
-  value--;
-  document.getElementById('number-two').value = value;
-}
-
-// For third product
-function increaseValueThree() {
-  var value = parseInt(document.getElementById('number-three').value, 10);
-  value = isNaN(value) ? 0 : value;
-  value++;
-  document.getElementById('number-three').value = value;
-}
-
-function decreaseValueThree() {
-  var value = parseInt(document.getElementById('number-three').value, 10);
-  value = isNaN(value) ? 0 : value;
-  value < 1 ? value = 1 : '';
-  value--;
-  document.getElementById('number-three').value = value;
-}
-
-// Rates Variables
-
-// var Apple = 10;
-// var Orange = 15;
-// var Banana = 7;
-
-var total_items = 4;
-
-function CalculateItemsValue() {
-    var total = 0;
-    for (i=1; i<=total_items; i++) {
-         
-        itemID = document.getElementById("number"+i);
-        if (typeof itemID === 'undefined' || itemID === null) {
-            alert("No such item - " + "number"+i);
-        } else {
-            total = total + parseInt(itemID.value) * parseInt(itemID.getAttribute("data-price"));
-        }
-         
     }
-    document.getElementById("ItemsTotal").innerHTML = "$" + total;
-     
+    else {
+        alert("Not Enough Money to buy Apple")
+    }
 }
+function increaseValueTwo() {
+
+    if (val >= 15) {
+        count_org += 1;
+        purchase += 15;
+        val = val - purchase;
+        total_purchase = purchase + total_purchase;
+        purchase = 0;
+        document.querySelector("#number-two").value = count_org; console.log(val);
+        result();
+
+    }
+    else {
+        alert("Not Enough Money to buy Orange")
+    }
+}
+function increaseValueThree() {
+    if (val >= 7) {
+        count_ban += 1;
+        purchase += 7;
+        val = val - purchase;
+        total_purchase = purchase + total_purchase;
+        purchase = 0;
+        document.querySelector("#number-three").value = count_ban; console.log(val);
+        result();
+
+    }
+    else {
+        alert("Not Enough Money to buy Banana")
+    }
+}
+function decreaseValue() {
+    if (total_purchase > 0 && count_ap > 0) {
+        count_ap -= 1;
+        purchase += 10;
+        val = val + purchase;
+        total_purchase = total_purchase - purchase;
+        purchase = 0;
+        document.querySelector("#number").value = count_ap;
+        result();
+    }
+
+}
+function decreaseValueTwo() {
+    if (total_purchase > 0 && count_org > 0) {
+        count_org -= 1;
+        purchase += 15;
+        val = val + purchase;
+        total_purchase = total_purchase - purchase;
+        purchase = 0;
+        document.querySelector("#number-two").value = count_org;
+        result();
+    }
+
+}
+function decreaseValueThree() {
+    if (total_purchase > 0 && count_ban > 0) {
+        count_ban -= 1;
+        purchase += 7;
+        val = val + purchase;
+        total_purchase = total_purchase - purchase;
+        purchase = 0;
+        document.querySelector("#number-three").value = count_ban;
+        result();
+    }
+}
+
+
 
